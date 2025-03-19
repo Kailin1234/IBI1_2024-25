09cb4b5f655e127e222cc7e54b8853583ad2db01
Kailin1234 <kailin.24@intl.zju.edu.cn>
2025-03-19 06:16:16 +0100

    add comments

--- Practical5/Population Sizes.py
+++ Practical5/Population Sizes.py
@@ -1,8 +1,8 @@
 # 1.Import matplotlib.pyplot to visualize statistic.
 # 2.Creat 2 figures.
 
-import matplotlib.pyplot as plt
-fig, (ax1, ax2) = plt.subplots(1, 2)
+import matplotlib.pyplot as plt # Import the pyplot module in matplotlib package. 
+fig, (ax1, ax2) = plt.subplots(1, 2) # Determine here will present 2 figures in 1 page.
 
 # 3.Input the labels of the first pie chart.
 # 4.Input relevant data and store it in a list.
@@ -10,13 +10,13 @@ fig, (ax1, ax2) = plt.subplots(1, 2)
 # 6.Determine the "explode" situation, every part isn't highlighted.
 # 7.Determine the pie chart is a circle.
 # 8.Input the title.
-labels1 = 'England', 'Wales', 'Northern Ireland', 'Scotland'
-uk_countries = [57.11, 3.13, 1.91, 5.45]
-sizes1 = uk_countries
-explode1 = (0, 0, 0, 0)
-ax1.pie(sizes1, explode=explode1, labels=labels1, autopct='%1.1f%%')
-ax1.axis('equal')
-ax1.set_title('UK')
+labels1 = 'England', 'Wales', 'Northern Ireland', 'Scotland' # Input the labels name of the first figure.
+uk_countries = [57.11, 3.13, 1.91, 5.45] # Create a list to store data of uk countries.
+sizes1 = uk_countries # Determine the sizes of the first figure, using the data stored.
+explode1 = (0, 0, 0, 0) # Every part isn't highlighted in this figure.
+ax1.pie(sizes1, explode=explode1, labels=labels1, autopct='%1.1f%%') # Dtermine the information of figure1, including the sizes, explode, labels and autopct.
+ax1.axis('equal') # Determine the figure is a circle.
+ax1.set_title('UK') # Determine the title.
 
 # 9.Input the labels of the second pie chart.
 # 10.Input relevant data and store it in a list.
@@ -24,15 +24,15 @@ ax1.set_title('UK')
 # 12.Determine the "explode" situation, every part isn't highlighted.
 # 13.Determine the pie chart is a circle.
 # 14.Input the title.
-labels2 = 'Zhejiang', 'Fujian', 'Jiangxi', 'Anhui', 'Jiangsu'
-China_provinces = [65.77, 41.88, 45.28, 61.27, 85.15]
-sizes2 = China_provinces
-explode2 = (0, 0, 0, 0, 0)
-ax2.pie(sizes2, explode=explode2, labels=labels2, autopct='%1.1f%%')
-ax2.axis('equal')
-ax2.set_title('China')
+labels2 = 'Zhejiang', 'Fujian', 'Jiangxi', 'Anhui', 'Jiangsu' # Input the labels name of the second figure.
+China_provinces = [65.77, 41.88, 45.28, 61.27, 85.15] # Create a list to store data of China provinces.
+sizes2 = China_provinces # Determine the sizes of the second figure, using the data stored.
+explode2 = (0, 0, 0, 0, 0) # Every part isn't highlighted in this figure.
+ax2.pie(sizes2, explode=explode2, labels=labels2, autopct='%1.1f%%') # Dtermine the information of figure2, including the sizes, explode, labels and autopct.
+ax2.axis('equal') # Determine the figure is a circle.
+ax2.set_title('China') # Determine the title.
 
 # 15.Determine the space between 2 pie charts, preventing coverage.
 # 16.Out these pie charts.
-plt.subplots_adjust(wspace=1)
-plt.show()
+plt.subplots_adjust(wspace=1) # Determine the space.
+plt.show() # Output the 2 figures
