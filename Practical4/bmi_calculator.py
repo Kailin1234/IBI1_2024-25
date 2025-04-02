a2a2170352919703b35ae5d77bcc527e85870df3
Kailin1234 <kailin.24@intl.zju.edu.cn>
2025-04-02 15:45:49 +0100

    modify

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -1,15 +1,17 @@
-# define the variables: weight, height, BMI
-# if the BMI value is greater than 30, the person is obese.
-# if the value is less than 18.5, the person is underweight.
-# if both not, the person is normal weight.
-# output the result
+# 1.If we need to let the user input their practical value, and we can calculate and get the result, we can use the function input()
+# 2.Because we need to further compare the value, but the input() function only can store data as string. We also need to use float() to change the data type. (We don't use int() because height value is often a float.)
+# 3.Let the user input his/her weight value and height value.
+# 4.Define the calculation of BMI.
+# 5.Use if sentense to determine whether BMI is bigger than 30, affecting whether output the obese result.
+# 6.Use elif sentense to determine whether BMI is smaller than 18.5, affecting whether determine whether BMI is smaller than 18.5.
+# 7.Use else sentense to consider the condition except for 2 conditions above, and output the normal weight result.
 
-weight = 46 # kg
-height = 1.63 # m
-BMI = weight/ height**2
-if BMI > 30:
-    print("Your BMI is", BMI, "You are considered obese")
-elif BMI < 18.5:
-    print("Your BMI is", BMI, "You are considered underweight")
-else:
-    print("Your BMI is", BMI, "You are considered normal weight")
+weight = float(input("Please enter your weight value. (kg)")) # Define variable weight.
+height = float(input("Please enter your height value. (m)")) # Define variable height.
+BMI = weight/height**2 # Define the calculation of BMI.
+if BMI > 30: # Determine whether BMI is bigger than 30.
+    print("Your BMI is", BMI, "You are considered obese") # If ture, output the obese result.
+elif BMI < 18.5: # Determine whether BMI is smaller than 18.5.
+    print("Your BMI is", BMI, "You are considered underweight") # If true, determine whether BMI is smaller than 18.5.
+else: # Consider the condition except for 2 conditions above.
+    print("Your BMI is", BMI, "You are considered normal weight") # Output the normal weight result.
