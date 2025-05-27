3fa5ef291a233b0ff5460b549785b8dc6ffda4af
Kailin1234 <kailin.24@intl.zju.edu.cn>
2025-05-27 12:35:49 +0100

    Add some comments

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,6 +1,12 @@
-import re
-seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
-largest_intron = re.findall(r'GT.+AG', seq)
-print(largest_intron)
-lenth = len(largest_intron[0])
-print(lenth)
+# 1.Import the re library to use regular expression.
+# 2.Define a new function named largest_intron, with paraments sequence.
+# 3.Use re.findall to find all occurrences of the pattern 'GT.+AG' in the sequence.
+# 4.Store the matches in a list called largest_intron.
+# 5.Print the largest intron found in the sequence.
+
+import re # Import the library
+seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA' # Exaple sequence
+largest_intron = re.findall(r'GT.+AG', seq) # Find the longest pattern 'GT.+AG' in the sequence
+print(largest_intron) # Output the largest intron
+lenth = len(largest_intron[0]) # Calculate the length of the largest intron
+print(lenth) # Output the length of the largest intron
