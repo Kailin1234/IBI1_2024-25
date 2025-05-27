7366fa380ada1fe50acb02fb2fd782c00c587ca4
Kailin1234 <kailin.24@intl.zju.edu.cn>
2025-05-27 14:01:56 +0100

    Ensure only 1 infected person at the beginning

--- Practical6/spatial_SIR.py
+++ Practical6/spatial_SIR.py
@@ -1,16 +1,20 @@
-# import necessary libraries
+# 1.import necessary libraries
 import numpy as np # import numpy library to process statistic
 import matplotlib.pyplot as plt # import matplotlib.pyplot library to visualize the statistic
 
-# set some basic variables and original population
+# 2.set some basic variables and original population
 beta = 0.3 # infected probability
 gamma = 0.05 # recovered probability
 population = np.zeros((100, 100)) # original population, 0 is not infected
-outbreak = np.random.choice(range(100),2) # pick 2 values for a hundres numbers as the coordinate
+outbreak = np.random.choice(range(100), 2) # pick 2 values for a hundres numbers as the coordinate
 population[outbreak[0], outbreak[1]] = 1 # the first infected individual
+plt.figure(figsize=(6,4),dpi = 150) # the basic information of this figure
+plt.imshow(population, cmap = 'viridis', interpolation = 'nearest') # import data population, set the color and the pixel situation
+plt.title("times = 0") # set the title of this figure
+plt.show() # output the figure
 
-# create a for loop to simulation SIR model, typically focusing on individuals
-for _ in range(101): # execute for 101 times, including 100 and not including 101
+# 3.create a for loop to simulation SIR model, typically focusing on individuals
+for t in range(101): # execute for 101 times, including 100 and not including 101
     new_population = population.copy() # create a copy of population to store changes
     Infected = np.where(population == 1) # select all the infected individuals, we get a tuble but not a list
     dx = [-1, 0, 1] # -1 is the left position, 0 is the original position and 1 is the right position
@@ -22,7 +26,7 @@ for _ in range(101): # execute for 101 times, including 100 and not including 10
                 di = i + dxi # select the individuals surrounding infected one
                 dj = j + dyi # select the individuals surrounding infected one
                 if 0 <=di < 100 and 0 <=dj <100: # ensure each individual is in the population but not outside
-                    if i == 0 and j == 0: # rule out the infected oneself
+                    if di == i and dj == j: # rule out the infected oneself
                         continue # continue select surrounding individuals
                     else: # the situation we can choose it as another infected object
                         if new_population[di,dj] == 0 and np.random.rand() < beta: # ensure it is a susceptible individual and use infected probability
@@ -30,7 +34,8 @@ for _ in range(101): # execute for 101 times, including 100 and not including 10
         if new_population[i, j] == 1 and np.random.rand() < gamma: # ensure it is an infected individual and use recovered probability
             new_population[i, j] = 2 # 2 is recovered
     population = new_population # update the population
-    if _ in [0, 10, 50,100]: # create figure through iteration
+    if t in [10, 50,100]: # create figure through iteration
         plt.figure(figsize=(6,4),dpi = 150) # the basic information of this figure
         plt.imshow(population, cmap = 'viridis', interpolation = 'nearest') # import data population, set the color and the pixel situation
+        plt.title(f"times = {t}") # set the title of this figure
         plt.show() # output the figure
