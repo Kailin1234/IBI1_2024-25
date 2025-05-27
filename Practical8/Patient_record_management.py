80ff13e59345a3aba51e2339f4cb0b04259d0e69
Kailin1234 <kailin.24@intl.zju.edu.cn>
2025-05-27 11:49:07 +0100

    Final submit

--- Practical8/Patient_record_management.py
+++ Practical8/Patient_record_management.py
@@ -20,3 +20,12 @@ class patients: # The new class.
 Patient1 = patients("Mark", 18, "2025/4/8", "None") # Use this class to store information of a patient.
 Information1 = Patient1.introduce() # Use the method(introduce) to access the information.
 print(Information1) # Output the patient's information.
+
+# Let the user input the data.
+name = input("Please enter the patient's name: ") # Let the user input the name.
+age = int(input("Please enter the patient's age: ")) # Let the user input the age. 
+date_of_lastest_admission = input("Please enter the date of lastest admission (YYYY/MM/DD): ") # Let the user input the date of lastest admission.
+medical_history = input("Please enter the patient's medical history: ") # Let the user input the medical history.  
+Patient2 = patients(name, age, date_of_lastest_admission, medical_history) # Use this class to store information of a patient.
+Information2 = Patient2.introduce() # Use the method(introduce) to access the information.
+print(Information2) # Output the patient's information.
